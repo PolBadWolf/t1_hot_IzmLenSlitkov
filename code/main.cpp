9c680f4eb5ed97c913b2ce8376bcaac03d85d8b2
BadWolf <gm.gameoff@gmail.com>
2014-10-17 14:50:55 +0600

    edit doc

--- code/main.cpp
+++ code/main.cpp
@@ -13,26 +13,26 @@ void main(void)
     void InitSystem(void);
     InitSystem();
   }
-  /*
+  
 //    ns_izmlen1::SendDebug();
   ns_izmlen1::datTimeMassive[0][0] = 1;
-  ns_izmlen1::datTimeMassive[0][1] = 20216;
-  ns_izmlen1::datTimeMassive[1][0] = 3278;
-  ns_izmlen1::datTimeMassive[1][1] = 23358;
-  ns_izmlen1::datTimeMassive[2][0] = 6362;
-  ns_izmlen1::datTimeMassive[2][1] = 26379;
-  ns_izmlen1::datTimeMassive[3][0] = 9427;
-  ns_izmlen1::datTimeMassive[3][1] = 29459;
-  ns_izmlen1::datTimeMassive[4][0] = 12334;
-  ns_izmlen1::datTimeMassive[4][1] = 33122;
-  ns_izmlen1::datTimeMassive[5][0] = 14942;
-  ns_izmlen1::datTimeMassive[5][1] = 38139;
-  ns_izmlen1::datTimeMassive[6][0] = 28236;
-  ns_izmlen1::datTimeMassive[6][1] = 61812;
-  ns_izmlen1::datTimeMassive[7][0] = 29720; //29268;
+  ns_izmlen1::datTimeMassive[0][1] = 0x4b5d;
+  ns_izmlen1::datTimeMassive[1][0] = 0x0b33;
+  ns_izmlen1::datTimeMassive[1][1] = 0x5768;
+  ns_izmlen1::datTimeMassive[2][0] = 0x167b;
+  ns_izmlen1::datTimeMassive[2][1] = 0x62de;
+  ns_izmlen1::datTimeMassive[3][0] = 0x223b;
+  ns_izmlen1::datTimeMassive[3][1] = 0x6fff;
+  ns_izmlen1::datTimeMassive[4][0] = 0x2ceb;
+  ns_izmlen1::datTimeMassive[4][1] = 0x8000;
+  ns_izmlen1::datTimeMassive[5][0] = 0x370e;
+  ns_izmlen1::datTimeMassive[5][1] = 0x92a1;
+  ns_izmlen1::datTimeMassive[6][0] = 0x69f4;
+  ns_izmlen1::datTimeMassive[6][1] = 0xe6a3;
+  ns_izmlen1::datTimeMassive[7][0] = 0x6fa7; //29268;
   ns_izmlen1::datTimeMassive[7][1] = 0;
     ns_izmlen1::IzmRenderMain();
-  */
+  
   _delay_ms(1000);
 //  scr->F_Digit_uz( (unsigned char)0, (unsigned char)4, (unsigned int)1234);
   __enable_interrupt();
